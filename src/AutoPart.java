dbdf64733ca667abf6e535243242078630068b5a
EztocCoun <vuduy30032004@gmail.com>
2024-09-01 20:15:00 +0700

    create autopart

---
+++ src/AutoPart.java
@@ -0,0 +1,105 @@
+public class AutoPart {
+    // Attributes
+    private String partID;
+    private String partName;
+    private String manufacturer;
+    private String partNumber;
+    private String condition; // new, used, refurbished
+    private String warranty;
+    private double cost;
+    private String notes;
+
+    // Constructor
+    public AutoPart(String partName, String manufacturer, String partNumber, String condition, String warranty, double cost, String notes) {
+        this.partID = generatePartID();
+        this.partName = partName;
+        this.manufacturer = manufacturer;
+        this.partNumber = partNumber;
+        this.condition = condition;
+        this.warranty = warranty;
+        this.cost = cost;
+        this.notes = notes;
+    }
+
+    // Generate a unique part ID (p-number format)
+    private String generatePartID() {
+        int number = (int) (Math.random() * 10000);
+        return "P-" + number;
+    }
+
+    // Getters and Setters
+    public String getPartID() {
+        return partID;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(String warranty) {
+        this.warranty = warranty;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    // toString method to display part information
+    @Override
+    public String toString() {
+        return "AutoPart{" +
+                "partID='" + partID + '\'' +
+                ", partName='" + partName + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", partNumber='" + partNumber + '\'' +
+                ", condition='" + condition + '\'' +
+                ", warranty='" + warranty + '\'' +
+                ", cost=" + cost +
+                ", notes='" + notes + '\'' +
+                '}';
+    }
+}
